b5b4194991a7c2c2ae01dfbff3a483aca8fbacbb
Lei Yang <septembernine@163.com>
2018-03-19 19:34:42 +0800

    add localhost match

--- src/python/change_ip.py
+++ src/python/change_ip.py
@@ -9,14 +9,14 @@ if len(sys.argv) < 3:
 elif len(sys.argv) == 3:
     f = fileinput.input(files=sys.argv[1], inplace=True, backup='.bak')
     for line in f:
-        line = re.sub('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', sys.argv[2], line)
+        line = re.sub('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost', sys.argv[2], line)
         line = line.rstrip();
         print(line)
     f.close()
 elif len(sys.argv) == 4:
     f = fileinput.input(files=sys.argv[1], inplace=True, backup='.bak')
     for line in f:
-        line = re.sub('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', sys.argv[2], line)
+        line = re.sub('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost', sys.argv[2], line)
         line = re.sub('TCP=\d*', "TCP="+sys.argv[3], line)
         line = line.rstrip();
         print(line)
