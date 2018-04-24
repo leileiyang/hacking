bb303ff519758c1b57f423d6d0fe7389948bc0b2
Lei Yang <septembernine@163.com>
2018-04-24 08:30:37 +0800

    sscanf literal match

--- src/draft/sscanf_test.c
+++ src/draft/sscanf_test.c
@@ -4,7 +4,7 @@
 
 int main() {
   const char *m_order = "nc_subroutines/M107 P0.111 Q9.111";
-  const char *m_order_no_args = "M107";
+  const char *m_order_no_args = "M107 (101)";
   char m_buf[30] = {0};
   char p_buf[30] = {0};
   char q_buf[30] = {0};
@@ -19,6 +19,11 @@ int main() {
   char c;
   int index;
   //sscanf(m_order_no_args, "%c%d %s %s", &c, &index, p_buf, q_buf);
-  sscanf(m_order_no_args, "%*[M]%d %s %s", &index, p_buf, q_buf);
-  printf("m:%d p:%f q:%f\n", index, atof(&p_buf[1]), atof(&q_buf[1]));
+  //sscanf(m_order_no_args, "%*[M]%d %s %s", &index, p_buf, q_buf);
+  char a;
+  char b;
+  int value;
+  sscanf(m_order_no_args, "M%d (%d) %s", &index, &value, p_buf);
+  //printf("m:%d p:%f q:%f\n", index, atof(&p_buf[1]), atof(&q_buf[1]));
+  printf("m:%d p:%d\n", index, value);
 }
