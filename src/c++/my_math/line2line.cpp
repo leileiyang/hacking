b31ffd15c37ccb4012e9fae0805d2c1cb7e466db
Lei Yang <septembernine@163.com>
2018-05-04 16:48:08 +0800

    add my_math project

---
+++ src/c++/my_math/line2line.cpp
@@ -0,0 +1,5 @@
+#include "line2line.h"
+
+Point_2D Line2Line(Line_2D line_a, Line_2D line_b) {
+  
+}
---
+++ src/c++/my_math/line2line.h
@@ -0,0 +1,18 @@
+#ifndef MY_MATH_LINE2LINE_H_
+#define MY_MATH_LINE2LINE_H_
+
+struct Point_2D {
+  double x;
+  double y;
+};
+
+struct Line_2D {
+  Point_2D start_point;
+  Point_2D end_point;
+};
+
+class line2line {
+
+};
+
+#endif // MY_MATH_LINE2LINE_H_
