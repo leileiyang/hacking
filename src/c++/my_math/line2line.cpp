b69fa8bb86630c343aea74bd13fa114a97118df0
Lei Yang <septembernine@163.com>
2018-05-04 16:59:25 +0800

    add DotPerp method

--- src/c++/my_math/line2line.cpp
+++ src/c++/my_math/line2line.cpp
@@ -1,5 +1,11 @@
 #include "line2line.h"
 
+double DotPerp(Point_2D point_a, Point_2D point_b) {
+  return point_a.x * point_b.y - point_b.x * point_a.y;
+}
+
+
+
 Point_2D Line2Line(Line_2D line_a, Line_2D line_b) {
   
 }
